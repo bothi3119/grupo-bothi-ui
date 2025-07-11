72983bd49ecd0c77b6877a67cff242ce699030c6
Jacinto Bothi <jacinto.bothi@origon.io>
2025-07-11 08:37:38 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grupo-bothi-ui',
+  webDir: 'www'
+};
+
+export default config;
